de09361f4c88f61f8a3e36a17c9f1a05058b0b85
emremutlu08 <mutluemre93@gmail.com>
2023-12-19 23:06:35 +0300

    fix: tailwind style problem is fixed

--- app/root.tsx
+++ app/root.tsx
@@ -21,6 +21,7 @@ import type {CustomerAccessToken} from '@shopify/hydrogen/storefront-api-types';
 import favicon from '../public/favicon.svg';
 import resetStyles from './styles/reset.css';
 import appStyles from './styles/app.css';
+import tailwindStyles from './styles/tailwind.css';
 import {Layout} from '~/components/Layout';
 
 /**
@@ -48,6 +49,7 @@ export function links() {
   return [
     {rel: 'stylesheet', href: resetStyles},
     {rel: 'stylesheet', href: appStyles},
+    {rel: 'stylesheet', href: tailwindStyles},
     {
       rel: 'preconnect',
       href: 'https://cdn.shopify.com',
